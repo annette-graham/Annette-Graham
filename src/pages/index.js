14216052628fa549b8e2e45f47da88dcc3150cfb
annette-graham <annette.graham@bbc.co.uk>
2020-06-19 17:09:18 +0100

    add home and about components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,22 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Header from '../components/Header/index.jsx'
+import Home from '../components/Home'
 import About from '../components/About'
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <Header 
-      className="sticky"
-    />
-    {/* <About /> */}
+    <Header />
+    <div className="main-body">
+      <div className='home'>
+        <Home />
+      </div>
+      {/* <div className="main-content">
+      </div> */}
+    </div>
+    <About />
     
   </Layout>
 )
