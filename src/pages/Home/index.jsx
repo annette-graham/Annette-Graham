8355e390b5ede27cb3a89ca23a2f433c9414596d
annette-graham <annette.graham@bbc.co.uk>
2020-07-03 14:32:01 +0100

    header and homepage almost done

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import sloth from '../../images/main-sloth.png'
+
+
+const Home = () => {
+  const arr = ['I AM ANNETTE AND I AM A SOFTWARE ENGINEER OKAY.', <br/>, 'GOSH']
+
+  return (
+    <section id="top" className="body home-body">
+      <div className='home-content'>
+        <img alt="Sloth doing tree pose" src={sloth} className='home-sloth' />
+        <h1 className="heading">
+          {arr}
+        </h1>
+        <p>Welcome to my website that I built for myself.</p>
+        <br/>
+        <a href='#/about' className='anchorLink'>Down Arrow</a>
+      </div>
+    </section>
+  )
+}
+
+export default Home
