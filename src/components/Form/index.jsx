d7f197e82f10fb0d1056fa4dc1b302c5500ec628
annette-graham <annette.graham@bbc.co.uk>
2020-07-10 16:14:35 +0100

    non-atomised form

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -5,67 +5,75 @@ import Input from '../Input'
 
 class Form extends React.Component {
   state = {
-    name: '',
-    email: '',
     message: ''
   }
 
   handleChange(e) {
-    const { name, value } = e.target
     this.setState({
-      [name]: value
+      message: e.target.value
     })
-    console.log(this.state)
   }
 
   handleSubmit(e) {
     e.preventDefault()
-    alert(`Submitted: ${this.state}`)
+    alert(`Submitted: ${this.state.message}`)
     // mailTo()
   }
 
   render() {
 
     return (
-        <div className='contact-form'>
-          <form name="contact" method="POST" data-netlify="true" onSubmit={e => this.handleSubmit(e)}>
-            <div className='input-group'> 
-               <Input
-                className="standard form-input"
-                tag="input"
-                label="Name"
-                name="Name"
-                placeholder="What's yo name fool?"
-                onChange={e => this.handleChange(e)}
-              />
-              <Input
-                className="standard form-input"
-                label="Email"
-                name="Email"
-                placeholder="example@email.com"
-                tag="input"
-                type='email'
-                onChange={e => this.handleChange(e)}
-              />
-              <Input
-                className="form-input"
-                label="Message"
-                name="Message"
-                placeholder="Omg Annette U da sh!t. We must hire you. I'll pay you £100k"
-                tag="textarea"
-                type='textarea'
-                onChange={e => this.handleChange(e)}
-              />
-              <Input
-                name="Submit"
-                tag="input"
-                type='submit'
-                className="submit form-input"
-              />
-            </div>
-          </form>
-          <img alt="Sloth reading a piece of paper" src={sloth} className='form-image' />
+      <div className='contact-form'>
+        <form 
+          name="contact" 
+          method="POST" 
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+          onSubmit={e => this.handleSubmit(e)}
+        >
+         <input type="hidden" name="form-name" value="contact" />
+          <div className="input-hidden">
+            <label htmlFor="bot-field">You're a bot ain't ya</label>
+            <input name="bot-field" id="bot-field" />
+          </div>
+          <div className='field'>
+            <label className='form-label'>Name</label>
+            <input 
+              className="standard form-input" 
+              name="name" 
+              placeholder="What's yo name fool?"
+            />
+          </div>
+          <div className='field'>
+            <label className='form-label'>Email</label>
+            <input 
+              className="standard form-input" 
+              name="Email"
+              placeholder="just ur email plz"
+              type="email" 
+            />
+          </div>
+          <div className='field'>
+            <label className='form-label'>Message</label>
+            <input 
+              className="standard form-input" 
+              name="Message" 
+              onChange={e => this.handleChange(e)} 
+              placeholder="What you gotta say?"
+              type="text" 
+            />
+          </div>
+          <div className='field'>
+            <label className='form-label'>Send</label>
+            <input 
+              className="submit form-input" 
+              name="send" 
+              type="submit" 
+            />
         </div>
+      </form>
+      <img alt="Sloth reading a piece of paper" src={sloth} className='form-image' />
+    </div>
     )
   }
 }
