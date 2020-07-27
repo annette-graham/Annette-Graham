1eaa99825805e186ed01275b03aca740702bad1f
annette-graham <annette.graham@bbc.co.uk>
2020-07-27 08:33:10 +0100

    refactored

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -5,27 +5,21 @@ import SocialMedia from '../../components/SocialMedia'
 import Form from '../../components/Form'
 
 
-const Contact = () => {
+const Contact = () => (
 
-  return (
-
-    <section id="/contact" className='body'>
-      <div className='body-content'>
-        <img alt="Sloth reading a piece of paper" src={sloth} className='reading-image' />
-          <h2 id="/contact" className="heading">Contact me bitch</h2>
-        {/* <div> */}
-          <Form />
-          <SocialMedia />
-        {/* </div> */}
-        <div>
-          <br/>
-          <p>Built using Create React App, Contentful, and Netlify. All rights are totz reserved.</p>
-          <p>© 2020 Annette Graham</p>
-        </div>
+  <section id="/contact" className='body'>
+    <div className='body-content'>
+      <img alt="Sloth reading a piece of paper" src={sloth} className='reading-image' />
+      <h2 id="/contact" className="heading">Contact me bitch</h2>
+      <Form />
+      <SocialMedia />
+      <div>
+        <br/>
+        <p>Built using Gatsby, Contentful, and Netlify. All rights are totz reserved.</p>
+        <p>© 2020 Annette Graham</p>
+      </div>
     </div>
-    </section>
-  )
-}
-
+  </section>
+)
 
 export default Contact
