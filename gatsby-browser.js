b007663e07f793d56021947bbc1ef197bf2b6706
annette-graham <annette.graham@bbc.co.uk>
2021-04-23 16:01:31 +0100

    add smooth scroll polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,5 +4,8 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
 import "./src/styles/_main.scss"
+import smoothscroll from 'smoothscroll-polyfill'
+
+// kick off the polyfill!
+smoothscroll.polyfill()
